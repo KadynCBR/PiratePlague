f066078e9cf6b626ad0de499e8fa83c866536c12
kadynCBR <kadyn_martinez@hotmail.com>
2024-01-12 13:05:10 -0800

    first commit

---
+++ Source/ProjectRogers/ProjectRogers.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectRogers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectRogers, "ProjectRogers" );
+ 
